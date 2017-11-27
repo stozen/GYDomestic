279cd2183adad13ecb3b7d235b446c7f9b54ff78
znlccy <756311868@qq.com>
2017-11-27 13:33:33 +0800

    增加微信第三方服务器向我们服务器发送支付结果

--- src/main/java/com/gy/pay/WeiXinPay.java
+++ src/main/java/com/gy/pay/WeiXinPay.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSONObject;
@@ -565,4 +566,50 @@ public class WeiXinPay {
 			}
         }
     }
+	 
+	 @RequestMapping(value="receivewx",method=RequestMethod.GET)
+	public @ResponseBody Map<String, Object> receiveWexin(@RequestParam String status,String out_trade_no){
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		
+		if(status.equals("0200"))
+		{
+			
+			PayRecord payRecord = payRecordService.get(out_trade_no);
+			if(payRecord==null)
+			{
+				status = "0604";
+				message = "没有这个订单";
+			}
+			else
+			{
+				message = "支付成功!";
+				status = "0200";
+				payRecord.setPayStatus("1");
+				payRecordService.update(payRecord);
+			}
+		}
+		else
+		{
+			PayRecord payRecord = payRecordService.get(out_trade_no);
+			if(payRecord==null)
+			{
+				status = "0604";
+				message = "没有这个订单";
+			}
+			else
+			{
+				message = "支付失败!";
+				status = "0605";
+				payRecord.setPayStatus("0");
+				payRecordService.update(payRecord);
+			}
+		}
+		
+		map.put("status",status);
+		map.put("message", message);
+		map.remove("out_trade_no");
+		
+		return map;
+	}
 }
